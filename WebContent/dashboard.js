7c04a68872c243276c4c688b2a3df3633c74f2af
vamsi-emani <vamsi2203@gmail.com>
2015-03-18 10:55:57 +0530

    Fixes #5. Moved creation of thumbnails to html.

--- WebContent/dashboard.js
+++ WebContent/dashboard.js
@@ -2,11 +2,7 @@
 	var dir = "events/";
 	var currentThumbnail;
 	var eventDetailCollpased = true;
-	var title = "<h2>Upcoming Events</h2>"
-	var thumbnailCSS = { "width" : "120px", "height" : "100px", 
-					     "padding" : "10px",
-						     /*"border":"1px solid #00ACED"	*/					     
-					    }
+	var title = "<h2>Upcoming Events</h2>"	
     /** Event name, date and venue info **/
 	var eventDetails = {
 		1 : "+ Women's day celebrations<br/><br/>+ 13th Feb 2015<br/><br/>+ Parade grounds",
@@ -22,31 +18,24 @@
 
 	/** for extended content - quotes/more info.. **/
 	var eventQuotes = {
-		"1" : "Whatever glory belongs to the race for a development unprecedented in history for the given length of time, a full share belongs to the womanhood of the race.<br/> <br/>~ Mary Mcleod Bethune",
-		"2" : "Music is a moral law. It gives soul to the universe, wings to the mind, flight to the imagination, and charm and gaiety to life and to everything.<br/><br/>~ Plato",
-		"3" : "Life is like dancing. If we have a big floor, many people will dance. Some will get angry when the rhythm changes. But life is changing all the time.<br/><br/>~ Miguel Angel Ruiz",
-		"4" : "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages.<br/><br/>~ W. Shakespeare",
-		"5" : "My dream concept is that I have a camera and I am trying to photograph what is essentially invisible. And every once in a while I get a glimpse of her and I grab that picture.<br/><br/>~ Leonard Nimoy",
-		"6" : "Acting is not about being someone different. It's finding the similarity in what is apparently different, then finding myself in there.<br/><br/>~ Meryl Streep", 
-		"7" : "Rock and Roll has no beginning and no end for it is the very pulse of life itself.<br/><br/>~ Larry Williams",
-		"8" : "Chess is like a language, the top players are very fluent at it. Talent can be developed scientifically but you have to find first what you are good at.<br/><br/>~ Vishwanath Anand",
-		"9" : "A day without laughter is a day wasted.<br/><br/>~ Charlie Chaplin"
+		1 : "Whatever glory belongs to the race for a development unprecedented in history for the given length of time, a full share belongs to the womanhood of the race.<br/> <br/>~ Mary Mcleod Bethune",
+		2 : "Music is a moral law. It gives soul to the universe, wings to the mind, flight to the imagination, and charm and gaiety to life and to everything.<br/><br/>~ Plato",
+		3 : "Life is like dancing. If we have a big floor, many people will dance. Some will get angry when the rhythm changes. But life is changing all the time.<br/><br/>~ Miguel Angel Ruiz",
+		4 : "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages.<br/><br/>~ W. Shakespeare",
+		5 : "My dream concept is that I have a camera and I am trying to photograph what is essentially invisible. And every once in a while I get a glimpse of her and I grab that picture.<br/><br/>~ Leonard Nimoy",
+		6 : "Acting is not about being someone different. It's finding the similarity in what is apparently different, then finding myself in there.<br/><br/>~ Meryl Streep", 
+		7 : "Rock and Roll has no beginning and no end for it is the very pulse of life itself.<br/><br/>~ Larry Williams",
+		8 : "Chess is like a language, the top players are very fluent at it. Talent can be developed scientifically but you have to find first what you are good at.<br/><br/>~ Vishwanath Anand",
+		9 : "A day without laughter is a day wasted.<br/><br/>~ Charlie Chaplin"
 
 	}
 	
-	/** Creates a row of thumbnail images displayed at the bottom **/
-	function createThumbnails(){
-		for(i=1; i<=9;i++){
-		  	var source = dir+i.toString()+".jpg";
-		  	var thumbnail = $('<img>',{id:i.toString(), src:source});
-		  	thumbnail.css(thumbnailCSS);			
-			$('.thumbnail-container').append(thumbnail);
-			thumbnail.on("click", function(){
-				currentThumbnail = this;
-				showImage(this);
-				displayEventDetails(this);				
-			});			
-		}  
+	function addThumbnailClickActions(){
+		$('.thumbnail').on("click", function(){			
+			currentThumbnail = this;
+			showImage(currentThumbnail);
+			displayEventDetails(currentThumbnail);				
+		});			
 	}
 
 	/** Invoked upon thumbnail click. Sets the thumbnail image as the photoframe **/
@@ -54,9 +43,14 @@
 		$('.main-container img').attr('src', $(thumbnail).attr('src'))		
 	}	
 	
+	function getKey(thumbnail){
+		var src = $(thumbnail).attr('src');
+		var key = src.substring(src.lastIndexOf("/")+1, src.lastIndexOf("."));
+		return parseInt(key);
+	}
 	/** To display event info **/
 	function displayEventDetails(thumbnail){
-		var key = $(thumbnail).attr('id');
+		var key = getKey(thumbnail);		
 		var expandCollpase = "<br/><br/><br/><b>Click to expand/collapse</b>"						
 		$('.event-detail').html(title+"<p> "+eventDetails[key]+expandCollpase+"</p>")	
 		if(!eventDetailCollpased)
@@ -81,7 +75,7 @@
 
 	/** Entry point **/
 	$( document ).ready(function() {				
-  		createThumbnails(); 	
+  		addThumbnailClickActions();
   		addEventDetailAnimations();   		  		
   		$('.extended-content').hide();  		
 	})	
